d2b0c1a634f6fd0d267ea8d043ec1ebea41e5e0b
Baard H. Rehn Johansen <baard@rehn.no>
2009-07-31 03:49:45 +0200

    added better start page

---
+++ src/no/rehn/android/trafikanten/SingleItemOverlay.java
@@ -0,0 +1,36 @@
+package no.rehn.android.trafikanten;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.widget.Toast;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+class SingleItemOverlay extends ItemizedOverlay<OverlayItem> {
+	final OverlayItem mItem;
+	final Context mContext;
+	public SingleItemOverlay(Context context, Drawable marker, OverlayItem item) {
+		super(boundCenterBottom(marker));
+		mItem = item;
+		mContext = context;
+		populate();
+	}
+	
+	 @Override
+	protected boolean onTap(int i) {
+		Toast.makeText(mContext, mItem.getTitle(), Toast.LENGTH_LONG)
+				.show();
+		return true;
+	}  
+	
+	@Override
+	protected OverlayItem createItem(int i) {
+		return mItem;  
+	}
+
+	@Override
+	public int size() {
+		return 1;
+	}
+}
